f804b860320089163a0675e3ddf7da2e96ad8802
DavidOwHoward <dairu81@gmail.com>
2026-01-25 16:10:46 -0500

    scoped locators for Details Screen. Created component page for Details Screen top toolbar

---
+++ components/DetailsTopToolBar.spec.ts
@@ -0,0 +1,32 @@
+import { Locator, type Page} from '@playwright/test';
+
+export class DetailsTopToolBar {
+
+    readonly page: Page;
+    readonly topToolBar: Locator;
+    readonly addButton: Locator;
+    readonly deleteButton: Locator;
+    readonly ellipseButton: Locator;
+    readonly reportsButton: Locator;
+    readonly actionsButton: Locator;
+    readonly attachmentsButton: Locator;
+    readonly commentsButton: Locator;
+
+    constructor(page: Page) {
+
+        this.page = page;
+        this.topToolBar = page.locator('eqms-details-layout')
+            .locator('.content')
+            .locator('.toolbar');
+
+
+        this.addButton = this.page.locator('#new-details-toolbar');
+        this.deleteButton = this.page.locator('#delete-details-toolbar');
+        this.ellipseButton = this.page.locator('#more-details-toolbar');
+        this.reportsButton = this.page.getByRole('button', { name: 'Reports' });
+        this.actionsButton = this.page.getByRole('button', { name: 'Actions' });
+        this.attachmentsButton = this.page.getByRole('button', { name: 'Attachments' });
+        this.commentsButton = this.page.locator('#comments-details-toolbar');
+    }
+
+}
