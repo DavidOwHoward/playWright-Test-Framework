f925fd59d8e6cacd11421f47fd99ceb9e3c218e0
DavidOwHoward <dairu81@gmail.com>
2026-01-26 16:26:08 -0500

    after testing, realized there were some issues. the favoritesIcon wasn't defined so it didn't know where to look for the mat-icon text. declared that and updated code accordingly the else statement was expecting 'outline' instead of 'star'. Tested and function does what is expected

--- utils/checkIfFavorite.spec.ts
+++ utils/checkIfFavorite.spec.ts
@@ -4,19 +4,20 @@ import {HomePage} from "../pages/HomePageBase.spec";
 export async function checkIfFavorite(
 
     page: Page,
-    locator: Locator,
+
     )
     {
 
     const home = new HomePage(page);
     const favorite = home.srch.favoriteButton
+    const favoriteIcon = home.srch.favoriteButton.locator('mat-icon')
     const menu = home.srch.ellipseButton
 
     //open the favorite menu
     await menu.click();
 
     //evaluate the icon text and store as iconBefore since it's before the first click action
-    const iconBefore = await favorite.evaluate(el => el.textContent.trim()
+    const iconBefore = await favoriteIcon.evaluate(el => el.textContent.trim()
     );
 
    //click the button
@@ -25,16 +26,14 @@ export async function checkIfFavorite(
    //since the menu closes after click, reopen the menu
    await menu.click();
 
-   //evalute the icon after the click and store it as icon after
-   const iconAfter = await favorite
-       .locator('mat-icon')
-       .evaluate(el => el.textContent.trim())
+   //evaluate the icon after the click and store it as icon after
+   const iconAfter = await favoriteIcon.evaluate(el => el.textContent.trim())
 
    if(iconBefore === 'star') {
        expect(iconAfter).toBe('star_outline')
 
    } else
-       expect(iconAfter).toBe('outline');
+       expect(iconAfter).toBe('star');
 
 
 }
