5ba71b79e5c30e1da70581836a66ec0a64f8f5a2
DavidOwHoward <dairu81@gmail.com>
2026-01-26 16:27:02 -0500

    removed unused Locator parameter

--- utils/checkIfFavorite.spec.ts
+++ utils/checkIfFavorite.spec.ts
@@ -1,4 +1,4 @@
-import { expect, Locator, Page } from '@playwright/test';
+import { expect, Page } from '@playwright/test';
 import {HomePage} from "../pages/HomePageBase.spec";
 
 export async function checkIfFavorite(
