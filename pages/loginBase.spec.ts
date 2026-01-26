4ce58b28ee404ade97e38c9d33712b9bf90b65c6
DavidOwHoward <dairu81@gmail.com>
2026-01-26 16:23:03 -0500

    added locator for the toast Welcome message so it can wait for it to go away. added the assert to check the title after login back to support the waiting for the toast message to go away. fixed some other formatting consistency issues

--- pages/loginBase.spec.ts
+++ pages/loginBase.spec.ts
@@ -7,6 +7,7 @@ export class loginPage {
     readonly userNameBox: Locator;
     readonly passwordBox: Locator;
     readonly signInButton: Locator;
+    readonly loginToast: Locator;
 
 
 
@@ -16,19 +17,25 @@ export class loginPage {
         this.userNameBox = page.getByLabel('User Name');
         this.passwordBox = page.getByLabel('Password');
         this.signInButton = page.getByRole("button", {name: 'Sign in'});
+        this.loginToast = page.locator('.cdk-overlay-pane')
+            .getByText('Welcome');
 
     }
     //TO DO: Turn these into one method
     async loginUser(user: string, password: string) {
-        await this.userNameBox.fill(user)
-        await this.passwordBox.fill(password)
+        await this.userNameBox.fill(user);
+        await this.passwordBox.fill(password);
         await this.signInButton.click();
+        await expect(this.page).toHaveTitle('Home - EQMS');
+    //wait for the toast to go away since some test run fast enough that it can be in the way of grabbing elements
+        await this.loginToast.waitFor({state: 'hidden'});
     }
 
     async openPage(){
         const url = 'https://homedev.qad.com'
         await this.page.goto(url);
         await expect(this.page).toHaveTitle('Login - QAD EQMS');
+
     }
 }
 
