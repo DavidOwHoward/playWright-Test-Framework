6bad7e20fb7eaa3b82513e3c92fe577408412b36
DavidOwHoward <dairu81@gmail.com>
2026-01-26 18:20:41 -0500

    added ReportsBase page

--- pages/HomePageBase.spec.ts
+++ pages/HomePageBase.spec.ts
@@ -3,12 +3,16 @@ import {SideNav} from "../components/sideNav";
 import {HomeTopToolBar} from "../components/HomeTopToolBar";
 import { LayoutsAndFiltersPanel} from "../components/filterAndLayoutPanel.spec";
 import { DetailsTopToolBar } from "../components/DetailsTopToolBar.spec";
+import { SearchScreen } from "../pages/SearchScreenBase.spec"
+import { ReportsBase} from "../components/ReportBase.spec";
 
 export class HomePage {
     readonly page: Page;
     readonly nav: SideNav;
     readonly httb: HomeTopToolBar
+    readonly srch: SearchScreen;
     readonly lfp: LayoutsAndFiltersPanel;
+    readonly reps: ReportsBase;
 
 
     constructor(page: Page) {
@@ -17,6 +21,8 @@ export class HomePage {
         this.nav = new SideNav(page);
         this.httb = new HomeTopToolBar(page);
         this.lfp = new LayoutsAndFiltersPanel(page);
+        this.srch = new SearchScreen(page);
+        this.reps = new ReportsBase(page);
     }
 
 
