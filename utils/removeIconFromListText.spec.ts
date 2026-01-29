db92856fd5b15e894f69ceebcfad4ae82f29f5c7
DavidOwHoward <dairu81@gmail.com>
2026-01-28 19:33:28 -0500

    helper function that I am hopinh I can reuse in the future to do list/menu content asserts. haven't been able to test it yet

---
+++ utils/removeIconFromListText.spec.ts
@@ -0,0 +1,18 @@
+import { Locator } from '@playwright/test';
+
+
+export async function getListTextWithoutIcons(listItems: Locator): Promise<string[]> {
+    return await listItems.evaluateAll((nodes) => {
+        return nodes.map((node) => {
+            //Make a deep copy so the DOM isn't modified
+            const clone = node.cloneNode(true) as Element;
+
+            //Remove all mat-icon elements from the clone
+            clone.querySelectorAll('mat-icon').forEach((icon) => icon.remove());
+
+            const text = clone.textContent;
+
+            return text
+        });
+    });
+}
