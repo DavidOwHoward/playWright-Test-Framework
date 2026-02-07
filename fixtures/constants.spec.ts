0233748d15f9ab614690433263d4937d7a96f1d9
DavidOwHoward <dairu81@gmail.com>
2026-02-07 00:34:03 -0500

    added an export for reports for temporary assertion. will be abstracted out into process page files when I get to that part

--- fixtures/constants.spec.ts
+++ fixtures/constants.spec.ts
@@ -1,3 +1,15 @@
 
 
 export const baseUrl = "https://homedev.qad.com";
+
+export const docReports = [
+    'Portrait',
+    'Portrait Fit To Page',
+    'Landscape',
+    'Landscape Fit To Page',
+    'Control Stamp Landscape',
+    'Control Stamp Portrait',
+    'Cover Page Portrait A4',
+    'Document Approval Time by Site',
+    'Employee Approval (by Site)'
+]
