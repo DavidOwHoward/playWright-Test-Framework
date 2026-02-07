6348368242205ddccb6f81742760ac0c474ba71b
DavidOwHoward <dairu81@gmail.com>
2026-02-07 12:37:45 -0500

    created a helper to get an access token for a class I need to work on later

---
+++ helpers/getTokenGenerator.ts
@@ -0,0 +1,36 @@
+import * as oidc from "openid-client";
+import { idpUrl } from "../fixtures/constants.spec";
+
+
+
+type TokenResult = {
+    access_token: string;
+    token_type?: string;
+    expires_in?: number;
+    scope?: string;
+};
+
+const ISSUER_BASE_URL = "https://homedev.qad.com/admintoolsidp";
+const CLIENT_ID = "cebos.internal.api";
+const CLIENT_SECRET = "fHeeOdPVDzXFTZxNST65FlKV02DaWfKKE+SeMOL3oyg=";
+
+
+export async function getAccessToken(): Promise<TokenResult> {
+    if (!CLIENT_ID || !CLIENT_SECRET) {
+        throw new Error("Missing client_id or client_secret is missing");
+    }
+
+    const config = await oidc.discovery(
+        new URL(ISSUER_BASE_URL),
+        CLIENT_ID,
+        CLIENT_SECRET
+    );
+
+    //  Client Credentials Grant
+    const tokenSet = await oidc.clientCredentialsGrant(config);
+
+    return tokenSet as TokenResult;
+
+}
+const displayToken = await getAccessToken();
+console.log(displayToken);
