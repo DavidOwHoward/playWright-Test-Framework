75551c2abe3640411a783f6df2664e5312122c1f
DavidOwHoward <dairu81@gmail.com>
2026-02-06 22:18:13 -0500

    re added the filterAndLayout locator. split the show and hide actions into their own methods. validated in an uncommited test file

--- components/SearchFilterRow.spec.ts
+++ components/SearchFilterRow.spec.ts
@@ -1,9 +1,10 @@
-import { Locator, Page } from '@playwright/test';
+import {expect, Locator, Page} from '@playwright/test';
 
 export class SearchFilterRow {
 
     readonly root: Locator;
     readonly page: Page;
+    readonly filterAndLayout: Locator;
     readonly filterRow: Locator;
     readonly showFilterRow: Locator;
     readonly hideFilterRow: Locator;
@@ -14,11 +15,24 @@ export class SearchFilterRow {
             .locator('eqms-grid')
             .locator('div[role="grid"][aria-label="Data table"]')
 
+        this.filterAndLayout = page.locator('#settings-search-toolbar');
         this.filterRow = this.root.locator('tr.k-filter-row');
         this.showFilterRow = page.getByRole('menuitem', { name: 'Show Filter Row' });
         this.hideFilterRow = page.getByRole('menuitem', { name: 'Hide Filter Row' });
 
     }
 
+    async FilterRowShow() {
+        await this.filterAndLayout.click();
+        await this.showFilterRow.click();
+        await expect(this.filterRow).toBeVisible();
+
+    }
+
+    async FilterRowHide() {
+        await this.filterAndLayout.click();
+        await this.hideFilterRow.click();
+        await expect(this.filterRow).toHaveCount(0);
+    }
 
 }
