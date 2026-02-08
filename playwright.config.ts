713f8731dc1a979efc9dcbd3b84fba6bb9e084e1
DavidOwHoward <dairu81@gmail.com>
2026-02-07 20:45:30 -0500

    updated to support .env file adoption

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig, devices } from '@playwright/test';
 
+
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
+import dotenv from 'dotenv';
 // import path from 'path';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+dotenv.config();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -48,6 +51,8 @@ export default defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
+
+
 
     /* Test against mobile viewports. */
     // {
