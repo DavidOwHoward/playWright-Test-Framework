bff1669cd4da44aed59696a1b372b6a9a45553ef
DavidOwHoward <dairu81@gmail.com>
2026-02-12 13:45:05 -0500

    changed assert to be more flexible in accepting any column to group by. added some comments and cleaned up formatting for easier reading

--- utils/dragAndDrop.ts
+++ utils/dragAndDrop.ts
@@ -1,5 +1,8 @@
 import { expect, type Page } from '@playwright/test';
 
+// function does all the actions needed to group data on the search screen and then asserts that the amount of grouped rows is greater than 0. Currently only supports grouping by 1 column.
+//TODO: refactor to group by more than one column
+
 
 export async function groupColumn(page: Page, column: string) {
         const anchor = page.locator('.k-drag-clue');
@@ -8,10 +11,15 @@ export async function groupColumn(page: Page, column: string) {
         await page.getByText(`${column}`).hover();
         await page.mouse.down();
         await page.waitForTimeout(2000);
+
         const box = await page.getByText(`${column}`).boundingBox();
-        if (!box) throw new Error('No bounding  box for column ' + column);
+        if (!box) throw new Error('No bounding box for column ' + column);
         await page.mouse.move(box.x + box.width / 2 + 20, box.y + box.height / 2);
+
         await expect(anchor).toBeVisible();
         await anchor.dragTo(target, {targetPosition: {x:100, y:30}, timeout: 3000});
-        await expect(page.locator('.k-grouping-row')).toHaveCount(5);
+        await page.waitForTimeout(2000);
+        const rowNum = await page.locator('.k-grouping-row').count()
+        // should consider what happens if a given column has no data. but that isn't a very realistic scenario so can save for later
+        expect(rowNum).toBeGreaterThan(0)
     }
