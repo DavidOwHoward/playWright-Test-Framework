0e5610cfce07f82a74dc6b9c0bae0a7bd7b082c0
DavidOwHoward <dairu81@gmail.com>
2026-02-16 17:15:32 -0500

    created a new folder for BasePages as the Pages folder is where process specific pages will live

--- basePages/DetailsPageBase.ts
+++ basePages/DetailsPageBase.ts
@@ -1,38 +1,43 @@
 import { Locator, type Page} from '@playwright/test';
+import { BasePage } from "./BasePage";
 
-export class DetailsPageBase {
+export class DetailsPageBase extends BasePage {
 
-    readonly page: Page;
+
     readonly footer: Locator;
     readonly closeButton: Locator;
     readonly saveButton: Locator;
     readonly saveNextButton: Locator;
     readonly stateSelector: Locator;
     readonly saveStateBar: Locator;
+    readonly stateDialog: Locator;
 
     constructor(page: Page)  {
 
-        this.page = page;
-        this.footer = this.page.locator('.footer-toolbar');
+        super(page);
+        this.footer = page.locator('.footer-toolbar');
         this.saveStateBar = this.footer
             .locator('.commands-container');
 
         this.saveButton = this.saveStateBar
             .getByRole('button', {name:'Save'});
-
         this.saveNextButton =this.saveStateBar
             .getByRole('button', {name:'Save & Next'});
-
         this.closeButton = this.saveStateBar
             .getByRole('button', { name: 'Close', exact: true })
-
         this.stateSelector = this.saveStateBar
             .getByRole('button', {name:'Active'});
+        this.stateDialog = page.locator('eqms-details-layout-dialog-confirm-state')
 
 };
 
     async closeRecord() {
         await this.closeButton.click();
+    }
+
+
+    async saveRecord() {
+        await this.saveButton.click();
     }
 
 }
