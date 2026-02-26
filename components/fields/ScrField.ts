16fa047bad047f9808b6ad4e66deaaf0852bc47d
DavidOwHoward <dairu81@gmail.com>
2026-02-25 19:50:32 -0500

    created scr field helper based on xref function in Basepage. validated with addDoc test

---
+++ components/fields/ScrField.ts
@@ -0,0 +1,52 @@
+import { expect } from "@playwright/test";
+import { FieldComponent } from "./FieldComponents";
+
+export class ScrField extends FieldComponent {
+
+    private dropDown() {
+        return this.root.getByRole('button').filter({ hasText: 'arrow_drop_down' });
+    }
+    
+    private input() {
+        return this.root.locator("input");
+    };
+
+    private listBox() {
+        return this.page.getByRole('listbox');
+    };
+
+
+    async open() {
+        await this.expectVisible();        
+        await this.dropDown().click();
+        await expect(this.listBox(), "SCR field dropdown did not open").toBeVisible();
+    };   
+    
+    async set(value: string) {
+
+        await this.open();
+        await this.dropDown().click();
+        await expect(this.listBox()).toBeVisible();
+        const option = this.page.getByRole('gridcell').getByText(value);
+        await expect(option, `Could not find "${value}" in the SCR dropdown options`).toBeVisible();
+        await option.click({force: true});
+        await expect(this.listBox()).not.toBeVisible();
+        
+    };
+
+    async expectValue(value:string, opts?: { assertContains?: string | RegExp}) {
+        await this.expectVisible();
+        const expected = opts?.assertContains ?? value
+
+        if (expected instanceof RegExp) {
+            await expect(this.input(), "SCR field value did not match").toHaveValue(expected);
+        } else {
+            const escaped = expected.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+            await expect(this.input(), "SCR field value did not match").toHaveValue(new RegExp (escaped, 'i'));
+        }
+    };
+
+
+
+
+};
