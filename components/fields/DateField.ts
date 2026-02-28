ed6f54afa8213f18bf4a4ad20da0d09a7f0ac0f8
DavidOwHoward <dairu81@gmail.com>
2026-02-28 17:28:48 -0500

    added DateField wrapper. works with only Today. will add a setText in future. Attempted to added methods to support picking a specific date within a specific month, but DOM is too volatile to make it consistent.

---
+++ components/fields/DateField.ts
@@ -0,0 +1,48 @@
+import { expect } from "@playwright/test";
+import { FieldComponent } from "./FieldComponents";
+
+
+export class DateField extends FieldComponent {
+  
+    private calendar() {
+        return this.page.locator(".k-calendar");
+    };
+  
+    private openCalendar() {
+    return this.root.getByRole('button', { name: 'Change Date' });
+  };
+
+    private today() {
+        return this.calendar().getByRole('link', { name: 'Today' });
+    }
+
+
+
+    // private setDate(day: string, month: number) {
+    //     return this.calendar().getByRole('grid').locator('tbody').nth(month).getByRole('gridcell').locator('.k-link').filter({ hasText: new RegExp(`\\b${day}\\b`)});
+    // }
+
+
+    async open() {
+        
+        await this.ensureReady();
+        await this.openCalendar().click();
+        await expect(this.calendar(), "Calendar did not open").toBeVisible();
+    }
+    
+    async setToday() {
+        
+        await this.ensureReady();
+        await this.open();
+        await this.today().click();
+        await expect(this.calendar()).not.toBeVisible();
+    };   
+
+    // async date(day: string, month: number) {
+                
+    //     await this.open();
+    //     await this.setDate(day, month).click();
+    //     await expect(this.calendar()).not.toBeVisible();
+
+    // };
+};
