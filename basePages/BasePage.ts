1759885688c8b09a9c501173e4eb78dd88e56f2b
DavidOwHoward <dairu81@gmail.com>
2026-02-28 13:07:01 -0500

    updated basePage and FieldComponent to support Sections and new Scr classes

--- basePages/BasePage.ts
+++ basePages/BasePage.ts
@@ -1,4 +1,5 @@
 import { expect, type Locator, type Page } from '@playwright/test';
+import { Sections } from '../components/Sections';
 
 export abstract class BasePage {
     protected readonly page: Page;
@@ -11,31 +12,42 @@ export abstract class BasePage {
     return this.page.locator(`#${id}_f`);
     };
 
-    async xref(field: Locator, value: string, opts?: { assertContains?: string | RegExp}) {
-
-        await field
-            .getByRole('button')
-            .filter({ hasText: 'arrow_drop_down' })
-            .click();
-
-        const listbox = this.page.getByRole('listbox');
-        await expect(listbox).toBeVisible();
-
-        const option = this.page.getByRole('gridcell').getByText(value);
-        await option.click();
-
-        await expect(listbox).not.toBeVisible();
-        const input = field.locator('input');
-        const expected = opts?.assertContains ?? value
-
-        if (expected instanceof RegExp) {
-
-            await expect(input).toHaveValue(expected);
-        } else {
-            const escaped = expected.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-            await expect(input).toHaveValue(new RegExp (escaped, 'i'));
-        }
+    protected sectionByName(name: string): Sections {
+        const tab = this.page.getByRole('tablist').locator(`#${name}-section-tab`);
+        return new Sections(this.page, tab);
     };
+
+    protected fieldRootInSection(fieldId: string, sectionName: string) {
+        const root = this.fieldRoot(fieldId);
+        const section  = this.sectionByName(sectionName);
+        return {root, section};
+    };
+
+    // async xref(field: Locator, value: string, opts?: { assertContains?: string | RegExp}) {
+
+    //     await field
+    //         .getByRole('button')
+    //         .filter({ hasText: 'arrow_drop_down' })
+    //         .click();
+
+    //     const listbox = this.page.getByRole('listbox');
+    //     await expect(listbox).toBeVisible();
+
+    //     const option = this.page.getByRole('gridcell').getByText(value);
+    //     await option.click();
+
+    //     await expect(listbox).not.toBeVisible();
+    //     const input = field.locator('input');
+    //     const expected = opts?.assertContains ?? value
+
+    //     if (expected instanceof RegExp) {
+
+    //         await expect(input).toHaveValue(expected);
+    //     } else {
+    //         const escaped = expected.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+    //         await expect(input).toHaveValue(new RegExp (escaped, 'i'));
+    //     }
+    // };
 
     async selectState(state: string) {
 
--- components/fields/FieldComponents.ts
+++ components/fields/FieldComponents.ts
@@ -1,7 +1,18 @@
 import { Locator, Page, expect } from "@playwright/test";
 
 export abstract class FieldComponent {
-    constructor(protected readonly page: Page, protected readonly root: Locator) {}
+    constructor(
+        protected readonly page: Page,
+        protected readonly root: Locator,
+        private readonly beforeOpen?: () => Promise<void>
+    ) {}
+
+    protected async ensureReady() {
+        if (this.beforeOpen) {
+            await this.beforeOpen();
+        }
+        await this.expectVisible();
+    }
 
     async expectVisible() {
         await expect(this.root, "Field wrapper not visible").toBeVisible();
