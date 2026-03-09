54277d853e02597ccec0d227c5ed6bae06af13ab
DavidOwHoward <dairu81@gmail.com>
2026-03-09 12:28:03 -0400

    modified test file to test new approval implementation

--- tests/addDoc.spec.ts
+++ tests/addDoc.spec.ts
@@ -1,24 +1,24 @@
 import { test, expect } from '../fixtures/fixtures'
 import { users } from "../config/user";
-import { Documents } from '../pages/Documnents_p'
-
+import { Documents_p } from '../pages/D/Documents_p'
 
 
 test('addDocRecord', async ({login, nav, search, page}) => {
 
-    const doc = new Documents(page);
+    const doc = new Documents_p(page);
     
     await login.openPage();
-    await login.loginUser(users.demo);
+    await login.loginUser(users.mgrDoc);
     await nav.navOpenProcess('Document Control', "Documents");
     await search.addNewRecord();
-    await doc.documentTitle.set('automated new doc');
-    await doc.documentType.set('General Documents');
-    await doc.documentType.expectValue('General Documents');
-    await doc.effectiveDate.set('03/03/2026');
-    await doc.documentFile.uploadFile('D:\\playWright\\sandbox\\testFiles\\David Howard - Allied.docx');  
-    await doc.saveRecord('Draft');
-
-    
+    await doc.DocumentTitle.set('approval test Charlie');
+    await doc.DocumentType.set('General Documents');
+    await doc.DocumentType.expectValue('General Documents');
+    await doc.EffectiveDate.set('03/09/2026');
+    await doc.DocumentFile.uploadFile('D:\\playWright\\sandbox\\testFiles\\David Howard - Allied.docx');  
+    await doc.saveRecord('Draft'); 
+    await doc.moveState('Draft', 'Awaiting Approval');
+    await doc.saveRecord();   
+    await doc.approveRecord(users.mgrDoc);
 
 });
