00376bd3a76cf457cdbcaee0a58d49d1817deb7f
DavidOwHoward <dairu81@gmail.com>
2026-03-10 12:04:11 -0400

    fixed button locators to use root instead of page. added link and unlink methods

--- components/fields/McrField.ts
+++ components/fields/McrField.ts
@@ -1,17 +1,18 @@
+import { expect } from "../../fixtures/fixtures";
 import { FieldComponent } from "./FieldComponents";
 
 export class McrField extends FieldComponent {
 
     private link() {
-        return this.page.getByRole('button').filter({ hasText: 'Link' });
+        return this.root.getByRole('button').filter({ hasText: 'Link' });
     };
 
     private showItems() {
-        return this.page.getByRole('button').filter({ hasText: 'reorder' });
+        return this.root.getByRole('button').filter({ hasText: 'reorder' });
     };
 
     private unlink() {
-        return this.page.getByRole('button').filter({ hasText: 'remove_circle_outline' });
+        return this.root.getByRole('button').filter({ hasText: 'remove_circle_outline' });
     };
 
     private linkWindow() {
@@ -34,13 +35,35 @@ export class McrField extends FieldComponent {
         return this.linkWindow().locator('.k-grid-table').getByRole('row').nth(0).getByRole('checkbox');
     };
 
+    private getRow() {
+        return this.linkWindow().locator('.k-grid-table').getByRole('row');
+    };
+
+
+    async setLink(value: string) {
+
+        this.ensureReady();
+        await this.link().click();
+        await this.linkSearch().fill(value);
+        await expect(this.getRow().first(), `Check for ${value}`).toContainText(value);
+        await this.clickFirst().click();
+        await expect(this.clickFirst(), 'Check if checked').toBeChecked();
+        await this.linkButton().click();
+    };
+
+    async unlinkItem(value: string) {
+
+        this.ensureReady();
+        const item = await this.root.getByRole('row').filter({ hasText: value });
+        await expect(item, `Check if ${value} is linked`).toBeVisible();
+        await item.click();
+        await expect(this.unlink(), 'Check if remove link is enabled').toBeEnabled();
+        await this.unlink().click();
+        await expect(item, `Check if ${value} is unlinked`).not.toBeVisible();
+    };
 
 
 
 
 
-
-
-
-
-}
+};
