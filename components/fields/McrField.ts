c6e8b48739772efdc289b2db52227894287fdca8
DavidOwHoward <dairu81@gmail.com>
2026-03-10 10:27:32 -0400

    first pass at Mcr field wrapper

--- components/fields/McrField.ts
+++ components/fields/McrField.ts
@@ -1,3 +1,46 @@
 import { FieldComponent } from "./FieldComponents";
 
-export class McrField extends FieldComponent {}
+export class McrField extends FieldComponent {
+
+    private link() {
+        return this.page.getByRole('button').filter({ hasText: 'Link' });
+    };
+
+    private showItems() {
+        return this.page.getByRole('button').filter({ hasText: 'reorder' });
+    };
+
+    private unlink() {
+        return this.page.getByRole('button').filter({ hasText: 'remove_circle_outline' });
+    };
+
+    private linkWindow() {
+        return this.page.getByRole('dialog')
+    };
+
+    private linkSearch() {
+        return this.linkWindow().getByRole('textbox');
+    };
+
+    private linkButton() {
+        return this.linkWindow().getByRole('button', {name: "Link"});
+    };
+
+    private cancel() {
+        return this.linkWindow().getByRole('button', {name: "Cancel"});
+    };
+
+    private clickFirst() {
+        return this.linkWindow().locator('.k-grid-table').getByRole('row').nth(0).getByRole('checkbox');
+    };
+
+
+
+
+
+
+
+
+
+
+}
