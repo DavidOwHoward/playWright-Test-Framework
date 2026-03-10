deda2709c5b9998769ee6a028ab8046539c4eaa9
DavidOwHoward <dairu81@gmail.com>
2026-03-09 22:31:02 -0400

    removed as no longer used/never actually developed

--- baseFramework/RunGenerator.ts
+++
@@ -1,8 +0,0 @@
-import {processIds} from '../fixtures/constants';
-import {generateProcessPages} from './PageObjectGenerator';
-import * as path from 'path';
-import * as fs from 'fs/promises';
-
-// replace the value to either be a single process or use processIds to generate for all processes in the db
-const allProcesses = processIds;
-await generateProcessPages([0]);
