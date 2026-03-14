a4e8625be66b4d7749ad78afd1db5d463dce88cb
DavidOwHoward <dairu81@gmail.com>
2026-03-14 18:03:48 -0400

    added file to use create a stable CLI command to run Cucumber with specific tags. it will run one at a time as its currently written, but that is fine for now. Updated package,json to support the new functionality

---
+++ src/cucumberRunner.ts
@@ -0,0 +1,38 @@
+import { exec } from 'child_process';
+import { tags } from './index';
+
+const common = `./src/features/*.feature \
+--require-module ts-node/register \
+--require ./src/step-definitions/**/*.ts \
+--require ./src/support/**/*.ts`;
+
+interface ProfileCommands {
+  [key: string]: string;
+};
+
+const profiles: ProfileCommands = {
+    navigation: `${common} --tags "@navigation"`,
+    login: `${common} --tags "@login"`
+};
+
+const profile = process.argv[2];
+
+if (!profile || !profiles[profile]) {
+    console.error(`Please provide a valid profile. Available profiles: ${Object.keys(profiles).join(', ')}`);
+    process.exit(1);
+};
+
+let command = `npx cucumber-js ${profiles[profile as 'navigation' | 'login']}`;
+
+exec(command, {encoding: 'utf-8'}, (error: Error | null, stdout: string) => {
+
+    console.log(stdout);
+
+    if(error) {
+        throw new Error('One or more tests have failed. Please review.');
+        process.exit(1);
+    }
+});
+
+
+
