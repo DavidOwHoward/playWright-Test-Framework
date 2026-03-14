192d01ea7cc63310d69cbc628b6880c22d4c24be
DavidOwHoward <dairu81@gmail.com>
2026-03-14 08:23:06 -0400

    created helper class to make working with test files easier

---
+++ fixtures/fileExports.ts
@@ -0,0 +1,11 @@
+import test from "@playwright/test";
+import path from "path";
+
+export const testFiles = {
+    classImage: path.resolve("D:\\playWright\\sandbox\\testFiles\\class.jpg"),
+    bathroomImage: path.resolve("D:\\playWright\\sandbox\\testFiles\\bathroom1.jpg"),
+    receptionImage: path.resolve("D:\\playWright\\sandbox\\testFiles\\reception.jpg"),
+    testDoc: path.resolve("D:\\playWright\\sandbox\\testFiles\\testDoc.docx"),
+    testPDF: path.resolve("D:\\playWright\\sandbox\\testFiles\\testPDF.pdf"),
+    testXLSX: path.resolve("D:\\playWright\\sandbox\\testFiles\\perceptron.xlsx")
+};
