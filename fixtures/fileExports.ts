b14c7e8862b8758616039125ffb74a5ef277ae2d
DavidOwHoward <dairu81@gmail.com>
2026-03-17 10:26:03 -0400

    added correct file paths for word and pdf files

--- fixtures/fileExports.ts
+++ fixtures/fileExports.ts
@@ -1,11 +1,10 @@
-import test from "@playwright/test";
 import path from "path";
 
 export const testFiles = {
     classImage: path.resolve("D:\\playWright\\sandbox\\testFiles\\class.jpg"),
     bathroomImage: path.resolve("D:\\playWright\\sandbox\\testFiles\\bathroom1.jpg"),
     receptionImage: path.resolve("D:\\playWright\\sandbox\\testFiles\\reception.jpg"),
-    testDoc: path.resolve("D:\\playWright\\sandbox\\testFiles\\testDoc.docx"),
-    testPDF: path.resolve("D:\\playWright\\sandbox\\testFiles\\testPDF.pdf"),
+    testDoc: path.resolve("D:\\playWright\\sandbox\\testFiles\\David Howard - Allied.docx"),
+    testPDF: path.resolve("D:\\playWright\\sandbox\\testFiles\\David Howard - FAC Services.pdf"),
     testXLSX: path.resolve("D:\\playWright\\sandbox\\testFiles\\perceptron.xlsx")
 };
