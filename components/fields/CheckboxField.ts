6df9e08523b22bfd93e70cfa65deab5a1752fbed
DavidOwHoward <dairu81@gmail.com>
2026-03-18 14:41:16 -0400

    implemented checkbox field wrapper

--- components/fields/CheckboxField.ts
+++ components/fields/CheckboxField.ts
@@ -1,3 +1,38 @@
+import { expect } from "../../fixtures/fixtures";
 import { FieldComponent } from "./FieldComponents";
 
-export class CheckboxField extends FieldComponent {}
+export class CheckboxField extends FieldComponent {
+
+    private checkbox() {
+        return this.root.getByRole('checkbox');
+    };
+
+    private toggle() {
+        return this.root.getByRole('radiogroup');
+    };
+
+    async check() {
+
+        await this.ensureReady();
+        await this.checkbox().check();
+        await expect(this.checkbox(), 'Assert the checkbox is checked').toBeChecked();
+
+    };
+
+    async uncheck() {
+
+        await this.ensureReady();
+        await this.checkbox().uncheck();
+        await expect(this.checkbox(), 'Assert the checkbox is unchecked').not.toBeChecked();
+
+    };
+
+    async setToggle(value: string) {
+
+        await this.ensureReady();
+        const option = this.toggle().getByRole('radio', {name: value});
+        await option.check();
+        await expect(option, `Assert the "${value}" toggle option is selected`).toBeChecked();
+
+    };
+};
