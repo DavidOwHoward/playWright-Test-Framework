92b3a44e0cb0c1d0b7b28da651f28500b5d8ffe1
DavidOwHoward <dairu81@gmail.com>
2026-03-21 18:27:17 -0400

    added a token URL constant and updated the webApi one to be more accurate

--- fixtures/constants.ts
+++ fixtures/constants.ts
@@ -3,7 +3,8 @@
 export const baseUrl = "https://homedev.qad.com";
 export const idpUrl = `${baseUrl}/admintoolsidp`;
 export const metaApi = `${baseUrl}/admintoolsapi`;
-export const webApi = `${baseUrl}/webapi/api/token`
+export const webApi = `${baseUrl}/webapi/api/`
+export const tokenApi = `${baseUrl}/Webapi/api/token`;
 
 export const docReports = [
     'Portrait',
