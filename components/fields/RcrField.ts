9692c41dee331c6073c1fa2f9baa88b2800c15e4
DavidOwHoward <dairu81@gmail.com>
2026-03-27 12:03:44 -0400

    added field wrapper code for RCR fields

--- components/fields/RcrField.ts
+++ components/fields/RcrField.ts
@@ -1,3 +1,18 @@
+import { expect } from "../../fixtures/tests";
 import { FieldComponent } from "./FieldComponents";
 
-export class RcrField extends FieldComponent {}
+export class RcrField extends FieldComponent {
+  private getTableRow() {
+    return this.root.locator(".k-grid-table").getByRole("row");
+  }
+
+  async open(value: string | RegExp) {
+    await this.ensureReady();
+    await this.getTableRow().filter({ hasText: value }).first().dblclick();
+  }
+
+  async assertValue(value: string | RegExp) {
+    await this.ensureReady();
+    await expect(this.getTableRow().filter({ hasText: value }).first()).toBeVisible();
+  }
+}
