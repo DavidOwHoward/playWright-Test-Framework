8772f610270c50ae6440b1fc80e397b2499af100
DavidOwHoward <dairu81@gmail.com>
2026-03-27 12:02:59 -0400

    added different test files

--- fixtures/fileExports.ts
+++ fixtures/fileExports.ts
@@ -4,7 +4,7 @@ export const testFiles = {
     classImage: path.resolve("D:\\playWright\\sandbox\\testFiles\\class.jpg"),
     bathroomImage: path.resolve("D:\\playWright\\sandbox\\testFiles\\bathroom1.jpg"),
     receptionImage: path.resolve("D:\\playWright\\sandbox\\testFiles\\reception.jpg"),
-    testDoc: path.resolve("D:\\playWright\\sandbox\\testFiles\\David Howard - Allied.docx"),
-    testPDF: path.resolve("D:\\playWright\\sandbox\\testFiles\\David Howard - FAC Services.pdf"),
+    testDoc: path.resolve("D:\\playWright\\sandbox\\testFiles\\Document_1.docx"),
+    testPDF: path.resolve("D:\\playWright\\sandbox\\testFiles\\lorem_ipsum.pdf"),
     testXLSX: path.resolve("D:\\playWright\\sandbox\\testFiles\\perceptron.xlsx")
 };
