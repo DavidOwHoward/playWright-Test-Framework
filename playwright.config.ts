42db6e610a03d757a4ea46746cb1171c2a63fbd6
DavidOwHoward <dairu81@gmail.com>
2026-03-28 11:51:56 -0400

    continuing to play with test timeout  values. 360000 is probably too high, but will re-evaluate at a later point in time

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
-  timeout: 180000,
+  timeout: 360000,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -66,7 +66,7 @@ export default defineConfig({
       use: {
         ...devices["Desktop Chrome"],
         viewport: { width: 1920, height: 1080 },
-        headless: false,
+        headless: true,
       },
     },
 
