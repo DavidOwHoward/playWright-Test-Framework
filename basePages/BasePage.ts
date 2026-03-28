7446cbbc99a94f2cbf18bfac7e4ae52fb8d181bb
DavidOwHoward <dairu81@gmail.com>
2026-03-28 13:43:55 -0400

    removed some old commented out code

--- basePages/BasePage.ts
+++ basePages/BasePage.ts
@@ -1,65 +1,32 @@
-import { expect, type Locator, type Page } from '@playwright/test';
-import { Sections } from '../components/Sections';
-import { Snackbar } from '../components/SnackBar';
+import { expect, type Locator, type Page } from "@playwright/test";
+import { Sections } from "../components/Sections";
+import { Snackbar } from "../components/SnackBar";
 
 export abstract class BasePage {
-    protected readonly page: Page;
-    protected readonly snack: Snackbar;
-    constructor(page: Page) {
-        this.page = page;
-        this.snack = new Snackbar(this.page);
-    }
+  protected readonly page: Page;
+  protected readonly snack: Snackbar;
+  constructor(page: Page) {
+    this.page = page;
+    this.snack = new Snackbar(this.page);
+  }
 
-    protected fieldRoot(id: string): Locator {
+  protected fieldRoot(id: string): Locator {
     return this.page.locator(`#${id}`);
-    };
+  }
 
-    protected sectionByName(name: string): Sections {
-        let tab: Locator;
-        if(name === '[Frozen Section]'|| name ==='Frozen Section') {
-		   tab = this.page.locator('#sticky');		
-		} else {
-			tab = this.page.getByRole('tablist').getByRole('tab', {name: name});
-			}
-        return new Sections(this.page, tab);
-    };
+  protected sectionByName(name: string): Sections {
+    let tab: Locator;
+    if (name === "[Frozen Section]" || name === "Frozen Section") {
+      tab = this.page.locator("#sticky");
+    } else {
+      tab = this.page.getByRole("tablist").getByRole("tab", { name: name });
+    }
+    return new Sections(this.page, tab);
+  }
 
-    // protected sectionByName(name: string): Sections {
-    //     const tab = this.page.getByRole('tablist').locator(`#${name}-section-tab`);
-    //     return new Sections(this.page, tab);
-    // };
-   
-
-    protected fieldRootInSection(fieldId: string, sectionName: string) {
-        const root = this.fieldRoot(fieldId);
-        const section  = this.sectionByName(sectionName);
-        return {root, section};
-    };
-
-
-    // async selectState(state: string) {
-
-    //     const root = this.page.locator('eqms-details-layout-dialog-confirm-state')
-    //     const stateButton = this.page.getByRole('button', {name: state})
-    //     await expect(root, 'The State popup was not visible').toBeVisible();
-    //     await expect(stateButton, `Could not find "${state}" in the State dialog`).toBeVisible();
-    //     await root.getByRole('button', {name: state}).click();
-    //     await this.saveConfirmation();
-    // };
-
-    // async saveConfirmation() {
-    //     await this.snack.waitForContains(/has saved/);
-    // };
-
-    // async snackVisible() {
-        
-    //     if (!(await (this.snack.message).isVisible())) {
-    //         return true;
-    //     }
-    // };
-
-
-
-};
-
-
+  protected fieldRootInSection(fieldId: string, sectionName: string) {
+    const root = this.fieldRoot(fieldId);
+    const section = this.sectionByName(sectionName);
+    return { root, section };
+  }
+}
