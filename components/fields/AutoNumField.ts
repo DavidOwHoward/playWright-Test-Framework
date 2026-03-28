e82013edc13a620c4c6dba58ba952e90c02a3361
DavidOwHoward <dairu81@gmail.com>
2026-03-28 13:46:18 -0400

    added some functionality to autonum to use in e2e testing

--- components/fields/AutoNumField.ts
+++ components/fields/AutoNumField.ts
@@ -1,3 +1,12 @@
 import { FieldComponent } from "./FieldComponents";
 
-export class AutoNumField extends FieldComponent {}
+export class AutoNumField extends FieldComponent {
+
+    private input() {
+        return this.root.locator('input');
+    }
+
+    async getValue() {
+        return await this.input().inputValue();
+    }
+}
