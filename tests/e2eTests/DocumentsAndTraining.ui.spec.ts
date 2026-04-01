72326c0e0cd1f24b5d2fdb56246902aa7dbae2c4
DavidOwHoward <dairu81@gmail.com>
2026-04-01 13:44:14 -0400

    final version of an e2e with tags and updated steps

--- tests/e2eTests/DocumentsAndTraining.ui.spec.ts
+++ tests/e2eTests/DocumentsAndTraining.ui.spec.ts
@@ -10,7 +10,7 @@ import { faker } from "@faker-js/faker";
 
 
 
-test("Documents and Training Workflow", async ({ actors }) => {
+test("Documents and Training Workflow", { tag: ['@e2e']}, async ({ actors }) => {
   const demo = await actors.as(users.demo);
 
   const doc = demo.process(Documents_p);
@@ -48,11 +48,11 @@ test("Documents and Training Workflow", async ({ actors }) => {
   await test.step("Add a Skill Training Question", async () => {
     await doc.SkillTrainingQuestions.addItem();
     await docSkill.Skill.assertVisibility();
-    await docSkill.Question.set("Does this pass?");
+    await docSkill.Question.set("Have you reviewed the document?");
     await docSkill.AnswerChoices.setGlobalChoice("Auditing - Pass/Fail");
     await docSkill.saveRecord("ACTIVE");
     await docSkill.closeRecord();
-    await doc.SkillTrainingQuestions.assertRowValue("Does this pass?");
+    await doc.SkillTrainingQuestions.assertRowValue("Have you reviewed the document?");
   });
 
   await test.step("Submit the Document for approval", async () => {
@@ -74,21 +74,23 @@ test("Documents and Training Workflow", async ({ actors }) => {
     await doc.approveRecord(users.demo, "Approving the document");
     await doc._TrainingEvents.assertValue("New version training for document: " + uniqueTitle);    
     await doc._TrainingEvents.open("New version training for document: " + uniqueTitle);    
-    await event.TrainingEventDescription.expectVisible();
+    await event.TrainingEventDescription.expectVisible();    
     await demo.home.userLogout();    
   });
 
-  await test.step("Log in as each user and complete the training log", async () => {
+  await test.step("Login as Carl Seragosa and complete the training log with approval", async () => {
     await mgr.login();
     await mgr.action.openAssignment("Training-Management","Training Event Awaiting Review", uniqueTitle);
     await mgrLogs.Employee.assertVisibility();
     await mgrLogs.TrainingEvaluationQuestions.selectAnswerBool("Pass");
     await mgrLogs.Comments.set("I have reviewed the document");
     await mgrLogs.saveRecord();
+    await mgrLogs.approveRecord(users.mgrDoc, "Approving the training log");
+    await mgr.home.userLogout();
 
   });
 
-  await test.step("Login as Cindy Smith and complete the training log", async () => {
+  await test.step("Login as Cindy Smith and complete the training log with approval", async () => {
     const mgr2 = await actors.as(users.mgrDoc2);
     const mgr2logs = mgr2.process(TrainingEventLogs_p);
     await mgr2.login();
@@ -97,10 +99,11 @@ test("Documents and Training Workflow", async ({ actors }) => {
     await mgr2logs.TrainingEvaluationQuestions.selectAnswerBool("Pass");
     await mgr2logs.Comments.set("I have reviewed the document");
     await mgr2logs.saveRecord();
+    await mgr2logs.approveRecord(users.mgrDoc2, "Approving the training log");
     await mgr2.home.userLogout();
   });
 
-  await test.step("Login as Jack Welch and complete the training log", async () => {
+  await test.step("Login as John Scott and complete the training log with approval", async () => {
     const quality1 = await actors.as(users.quality1);
     const quality1logs = quality1.process(TrainingEventLogs_p);
     await quality1.login();
@@ -109,10 +112,11 @@ test("Documents and Training Workflow", async ({ actors }) => {
     await quality1logs.TrainingEvaluationQuestions.selectAnswerBool("Pass");
     await quality1logs.Comments.set("I have reviewed the document");
     await quality1logs.saveRecord();
+    await quality1logs.approveRecord(users.quality1, "Approving the training log");
     await quality1.home.userLogout();
   });
 
-  await test.step("Login as Audit and complete the training log", async () => {
+  await test.step("Login as Phil Copper and complete the training log with approval", async () => {
     const audit = await actors.as(users.Audit);
     const auditlogs = audit.process(TrainingEventLogs_p);
     await audit.login();
@@ -121,18 +125,24 @@ test("Documents and Training Workflow", async ({ actors }) => {
     await auditlogs.TrainingEvaluationQuestions.selectAnswerBool("Pass");
     await auditlogs.Comments.set("I have reviewed the document");
     await auditlogs.saveRecord();
+    await auditlogs.approveRecord(users.Audit, "Approving the training log");
     await audit.home.userLogout();
   }); 
 
-  await test.step("Login as Jack Welch and verify the training log is completed", async () => {
+  await test.step("Login as Jack Welch and complete the training log with approval", async () => {
     const quality = await actors.as(users.quality);
     const qualityLogs = quality.process(TrainingEventLogs_p);
+    const qualityEvent = quality.process(TrainingEvents_p);
+
     await quality.login();
     await quality.action.openAssignment("Training-Management","Training Event Awaiting Review", uniqueTitle);
     await qualityLogs.Employee.assertVisibility();
     await qualityLogs.TrainingEvaluationQuestions.selectAnswerBool("Pass");
     await qualityLogs.Comments.set("I have reviewed the document");
     await qualityLogs.saveRecord();
+    await qualityLogs.approveRecord(users.quality, "Approving the training log");
+    await qualityLogs.TrainingEvent.showItem();
+    await qualityEvent.assertState("Closed");
     await quality.home.userLogout();
   });
 
