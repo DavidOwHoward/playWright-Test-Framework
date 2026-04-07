07186927d280fc4b21695909e0de15dea681eb3c
DavidOwHoward <dairu81@gmail.com>
2026-04-07 13:35:47 -0400

    changed import to be more flexible

--- tests/apitests/addDocument.api.spec.ts
+++ tests/apitests/addDocument.api.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 import { users } from "../../config/user";
 import { docPayload } from "../../testFiles/apiPayloads/addDocumentRecord";
-import { addRecordApi, openRecordApi } from "../../helpers/api/AddRecordApi";
+import * as apiCall from "../../helpers/api/AddRecordApi";
 import * as apiExpect from "../../helpers/api/ApiAssertions";
 
 test(
@@ -10,21 +10,21 @@ test(
   async ({ request }) => {
     let id: number;
     await test.step("Add a Document record via the WebAPI", async () => {
-      const response = await addRecordApi(users.mgrDoc, "Documents_p", docPayload);
+      const response = await apiCall.addRecordApi(users.mgrDoc, "Documents_p", docPayload);
       
       apiExpect.expectOk(response);
       id = response.data.sections[0].fields[0].id;      
     });
 
     await test.step("Validate that a user without permissions cannot view the Document record", async () => {      
-      const response = await openRecordApi(users["31S1002QM"], "Documents_p", id);
+      const response = await apiCall.openRecordApi(users["31S1002QM"], "Documents_p", id);
       
       apiExpect.expectForbidden(response);
     });
 
     await test.step("Validate that a user with permissions can view the Document record", async () => {
       
-      const response = await openRecordApi(users.demo, "Documents_p", id);
+      const response = await apiCall.openRecordApi(users.demo, "Documents_p", id);
       
       apiExpect.expectOk(response);
     });
